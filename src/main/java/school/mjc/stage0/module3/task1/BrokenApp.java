1e8814f15474b1e1a702725a1ab5e3616b0bf5e3
shenanigami <38479116+shenanigami@users.noreply.github.com>
2023-08-23 00:21:23 +0600

    Again trying a different approach with BrokenApp

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class BrokenApp {
     public static void main(String[] args) {
-        System.out.println("Fix\nMe");
+        System.out.println("fixMe");
     }
 }
